ddcb684d94bf045cffd28a70fa59d1987c5e355f
lakshya <lakshya_jain@epam.com>
2025-04-04 15:10:58 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; 
+
 const heading = React.createElement("h1", {}, "Hello World from React...!!");
 console.log(heading);
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
