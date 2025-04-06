da26e0356753b234c9245869af4833c22ca42b1e
lakshya <lakshya_jain@epam.com>
2025-04-06 19:49:15 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; 
+import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1", {}, "Hello World from React...!!");
-console.log(heading);
+
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));
-// console.log(rootElement);
-rootElement.render(heading);
+const jsxHeading=<h1>Hello world from the JSX...!!!</h1>;
+// rootElement.render(heading);
+// rootElement.render(jsxHeading);
+const reactElement=(
+    <h2>Hello world from React Element</h2>
+);
+const FunctionalComponent =()=>{
+    return <div>
+        {reactElement}
+    <h1>Hello world from Functional Component</h1>
+
+    </div>
+    
+};
+rootElement.render(<FunctionalComponent/>);
